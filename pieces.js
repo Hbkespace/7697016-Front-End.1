4020db39b5f40a4c314e6cc37ef33d811d07a922
Hbkespace <franckyjohn.parisel@gmail.com>
2025-04-14 13:54:21 +0000

    entrainement exercice

---
+++ pieces.js
@@ -0,0 +1,26 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json")
+const pieces = await reponse.json()
+
+// création de la liste des éléments de la page
+const article = pieces[0]
+
+const imageElement = document.createElement("img")
+imageElement.src = article.image
+
+const nomElement = document.createElement("h2")
+nomElement.innerText = article.nom
+
+const prixElement = document.createElement("p")
+prixElement.innerText = `Prix : ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`
+
+const categorieElement = document.createElement("p")
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)"
+
+// rattachement des elements au code HTML parent de la page ou l'on affiche les elements
+const sectionFiches = document.querySelector(".fiches")
+
+sectionFiches.appendChild(imageElement)
+sectionFiches.appendChild(nomElement)
+sectionFiches.appendChild(prixElement)
+sectionFiches.appendChild(categorieElement)
